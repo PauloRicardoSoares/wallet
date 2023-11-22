95945e554def217edc63c2417bc4661aee90d41d
Paulo Ricardo <97540014+PauloRicardoSoares@users.noreply.github.com>
2023-11-22 09:44:48 +0000

    .

--- components/TransitionCard.jsx
+++ components/TransitionCard.jsx
@@ -15,33 +15,32 @@ const PromptCard = ({ transition }) => {
 
   return (
     <div className="prompt_card">
+      <div>
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">
-              {transition.description}
-            </h3>
-            <p
-              className="font-inter text-sm blue_gradient cursor-pointer"
-              onClick={() => handleTagClick && handleTagClick(transition.tag)}
-            >
-              {transition.tag}
-            </p>
-          </div>
-        </div>
+        <h3 className="font-satoshi font-semibold text-gray-900">
+          {transition.description}
+        </h3>
+        <p
+          className="font-inter text-sm blue_gradient cursor-pointer"
+          onClick={() => handleTagClick && handleTagClick(transition.tag)}
+        >
+          {transition.tag}
+        </p>
       </div>
 
-{transition.type === "I" ? (
-
-
-      <p className="my-4 font-satoshi text-sm text-green-700">
-        {parseFloat(transition.value.$numberDecimal).toFixed(2)}
-      </p>
-) : (
-  <p className="my-4 font-satoshi text-sm text-red">
-    {parseFloat(transition.value.$numberDecimal).toFixed(2)}
-  </p>
-)}
+      {transition.type === "I" ? (
+        <p className="my-4 font-satoshi text-sm text-green-700">
+          {parseFloat(transition.value.$numberDecimal).toFixed(2)}
+        </p>
+      ) : (
+        <p className="my-4 font-satoshi text-sm text-red-700">
+          {parseFloat(transition.value.$numberDecimal).toFixed(2)}
+        </p>
+      )}
+      </div>
+      <div className="text-red-700">
+        Deletar
+      </div>
     </div>
   );
 };
