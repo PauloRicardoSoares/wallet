2e39bc80683a75dd3fb5c5e036e42c6b3dd7e656
Paulo Ricardo <97540014+PauloRicardoSoares@users.noreply.github.com>
2023-11-22 15:11:53 +0000

    .

--- components/TransitionCard.jsx
+++ components/TransitionCard.jsx
@@ -13,32 +13,36 @@ const PromptCard = ({ transition }) => {
 
   const [copied, setCopied] = useState("");
 
+  const handleDeleteClick = (e) => {
+    alert(transition.description);
+  };
+
   return (
     <div className="prompt_card flex flex-col">
       <div>
-      <div className="flex flex-col justify-between items-start gap-5">
-        <h3 className="font-satoshi font-semibold text-gray-900">
-          {transition.description}
-        </h3>
-        <p
-          className="font-inter text-sm blue_gradient cursor-pointer"
-          onClick={() => handleTagClick && handleTagClick(transition.tag)}
-        >
-          {transition.tag}
-        </p>
-      </div>
+        <div className="flex flex-col justify-between items-start gap-5">
+          <h3 className="font-satoshi font-semibold text-gray-900">
+            {transition.description}
+          </h3>
+          <p
+            className="font-inter text-sm blue_gradient cursor-pointer"
+            onClick={() => handleTagClick && handleTagClick(transition.tag)}
+          >
+            {transition.tag}
+          </p>
+        </div>
 
-      {transition.type === "I" ? (
-        <p className="my-4 font-satoshi text-sm text-green-700">
-          {parseFloat(transition.value.$numberDecimal).toFixed(2)}
-        </p>
-      ) : (
-        <p className="my-4 font-satoshi text-sm text-red-700">
-          {parseFloat(transition.value.$numberDecimal).toFixed(2)}
-        </p>
-      )}
+        {transition.type === "I" ? (
+          <p className="my-4 font-satoshi text-sm text-green-700">
+            {parseFloat(transition.value.$numberDecimal).toFixed(2)}
+          </p>
+        ) : (
+          <p className="my-4 font-satoshi text-sm text-red-700">
+            {parseFloat(transition.value.$numberDecimal).toFixed(2)}
+          </p>
+        )}
       </div>
-      <div className="text-red-700">
+      <div className="text-red-700" onClick={() => handleDeleteClick()}>
         Deletar
       </div>
     </div>
